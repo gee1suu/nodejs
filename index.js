9c1027962fccd3ed0dabe0fb663e0027adcbfb56
gee1suu <therese86126@ewhain.net>
2022-03-31 20:20:33 +0900

    #3 몽고 DB 연결

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+mongoose
+  .connect(
+    "mongodb+srv://gee1suu:today0929@boilerplate.nacnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((error) => console.log(err));

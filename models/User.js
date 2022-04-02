9df8df6ae4bff04395b581d867fdacde54f1a4e3
gee1suu <therese86126@ewhain.net>
2022-04-02 15:45:25 +0900

    #11 로그인 기능 with Bcrypt (1)

--- models/User.js
+++ models/User.js
@@ -46,8 +46,18 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+userSchema.methods.comarePassword = function (plainPassword, cb) {
+  // plainPW를 암호화시켜서, DB에 있는 암호화된 PW와 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 

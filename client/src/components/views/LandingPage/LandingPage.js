992ac8f2f00dab46a865db4a842729834b2e1dda
gee1suu <therese86126@ewhain.net>
2022-04-06 19:35:51 +0900

    #21 데이터 Flow & Axios

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 export default function LandingPage() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/hello")
+      .then((response) => console.log(response.data));
+  }, []);
   return <div>LandingPage</div>;
 }

7ea266b84f0abe8e7f1d190c5df28c1ea29d21fa
gee1suu <therese86126@ewhain.net>
2022-04-06 19:21:49 +0900

    #20 React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }

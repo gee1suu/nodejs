9ed568e9eecd3bc1a85453383ee2967f63a096dd
gee1suu <therese86126@ewhain.net>
2022-04-12 21:04:32 +0900

    #29 로그인 페이지 (1)

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,3 +1,35 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.currentTarget.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.currentTarget.value)}
+        />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }

9603bf5a442856279056fe3a1db653f6eb352605
gee1suu <therese86126@ewhain.net>
2022-04-18 18:44:35 +0900

    #31 회원 가입 페이지

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get("/api/hello", (req, res) => {
   res.send("안녕하세요~");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
   user.save((err, userInfo) => {
